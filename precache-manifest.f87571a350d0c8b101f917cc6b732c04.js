9230d451196ab233273e179cacc7635371fa316b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-21 04:33:03 +0000

    Deploy maxime-rainville/travis-dashboard to github.com/maxime-rainville/travis-dashboard.git:gh-pages

---
+++ precache-manifest.f87571a350d0c8b101f917cc6b732c04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf02fa0e82cfee3b8a58bbea94d503f",
+    "url": "/travis-dashboard/index.html"
+  },
+  {
+    "revision": "d49c9364442b714fca45",
+    "url": "/travis-dashboard/static/js/2.3f97d46c.chunk.js"
+  },
+  {
+    "revision": "c8a4a051cf0d667b7f8d94baeb475206",
+    "url": "/travis-dashboard/static/js/2.3f97d46c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291ebcd8eb61548b7476",
+    "url": "/travis-dashboard/static/js/main.5497520d.chunk.js"
+  },
+  {
+    "revision": "6fae7c62261fd0b6ecd0",
+    "url": "/travis-dashboard/static/js/runtime-main.c4278627.js"
+  }
+]);
